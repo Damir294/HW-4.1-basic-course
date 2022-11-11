6613d7dfb377731a937a2d4a2063a57429b5f172
Дамир <damir.obuhov@mail.ru>
2022-11-11 09:09:45 +0400

    Цикл For  часть 1 задачи с 1 по 4-ю

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+// задача 1
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10;  i++);{
+            System.out.println("i"=i+);
+        }
+    }
+}
+ // задача 2
+public class Main {
+     public  static void  main(String[] args){
+       for (int i=1; i >=10; i--){
+         System.out.println("i"= i+);
+       }
+     }
+ }
+ // здача 3
+public  class  Main{
+     public static void  main(String[] args){
+         for (int i=0; i<=17; i+2);
+     } System.out.println("i"= i+);
+
+
+
+// задача 4
+public class Maim{
+ public static void  main(String[] args){
+   for (int i=10; i>= -10; i--);
+   System.out.println(i);
+ }
+}
+ }
